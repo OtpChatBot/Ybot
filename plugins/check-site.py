17b23f7b30e87ead89cbdd51401e8c4e1dcfe55d
0xAX <testtest.89@mail.ru>
2013-01-23 00:25:23 +0600

    check-site.py plugin added

---
+++ plugins/check-site.py
@@ -0,0 +1,26 @@
+"""
+Ybot check site up/down
+
+Usage:
+
+  Ybot check-site http://example.com 
+
+"""
+
+import sys
+import urllib
+import urllib2
+
+# Get url for checking
+url = sys.argv[1]
+# Send request
+response = urllib2.urlopen('http://www.isup.me/' + url).read()
+# check site up or down
+check_site = 'It\'s just you' in response
+
+# Return
+if check_site == True:
+	print "Site is up"
+else
+	print "Site is down"
+
