8b925951d962be8831531558720424386e4d4665
Martin Wiso <martin.wiso@sumup.com>
2013-03-07 09:31:08 +0100

    Prototyping REST API using cowboy_rest

--- src/ybot_brain_api.erl
+++ src/ybot_brain_api.erl
@@ -1,8 +1,8 @@
-%%%----------------------------------------------------------------------
+%%%----------------------------------------------------------------------------
 %%% File    : ybot_brain_api.erl
 %%% Author  : tgrk <tajgur@gmail.com>
 %%% Purpose : Ybot brain REST API
-%%%----------------------------------------------------------------------
+%%%----------------------------------------------------------------------------
 -module(ybot_brain_api).
 
 -include("ybot.hrl").
@@ -12,13 +12,14 @@
          allowed_methods/2,
          content_types_provided/2,
          content_types_accepted/2,
-         resource_exists/2,
+%         resource_exists/2,
          post_is_create/2,
-         create_path/2,
-         post_json/2,
-         put_json/2,
+         created_path/2,
+         process_post/2,
+         create_json/2,
          get_json/2,
-         delete_json/2
+         delete_resource/2,
+         delete_completed/2
         ]).
 
 %% API
@@ -26,11 +27,11 @@ init(_Transport, _Req, []) ->
     {upgrade, protocol, cowboy_rest}.
 
 allowed_methods(Req, State) ->
-    {[<<"GET">>, <<"POST">>, <<"PUT">>, <<"DELETE">>], Req, State}.
+    {[<<"HEAD">>, <<"GET">>, <<"POST">>, <<"PUT">>, <<"DELETE">>], Req, State}.
 
 content_types_accepted(Req, State) ->
     {[
-      {{<<"application">>, <<"json">>, []}, put_json}
+      {{<<"application">>, <<"json">>, []}, create_json}
      ], Req, State}.
 
 content_types_provided(Req, State) ->
@@ -38,37 +39,26 @@ content_types_provided(Req, State) ->
       {{<<"application">>, <<"json">>, []}, get_json}
      ], Req, State}.
 
-%% resource_exists(Req, _State) ->
-%%     case cowboy_req:binding(paste_id, Req) of
-%%         {undefined, Req2} ->
-%%             {true, Req2, index};
-%%         {PasteID, Req2} ->
-%%             case valid_path(PasteID) and file_exists(PasteID) of
-%%                 true -> {true, Req2, PasteID};
-%%                 false -> {false, Req2, PasteID}
-%%             end
-%%     end.
-resource_exists(Req, State)->
-    lager:info("api:resource_exists", []),
-    {true, Req, index}.
+%% resource_exists(Req, State)->
+%%     lager:info("api:resource_exists", []),
+%%     {true, Req, index}.
 
 post_is_create(Req, State) ->
-    lager:info("api:post_is_create", []),
     {true, Req, State}.
 
-create_path(Req, State) ->
-    lager:info("api:create_path", []),
+created_path(Req, State) ->
+    lager:info("api:created_path", []),
     {ybot_utils:get_uuid(), Req, State}.
 
-put_json(Req, State) ->
+create_json(Req, State) ->
     {Id, Req1} = cowboy_req:meta(put_path, Req),
-    {ok, [{Json, true}], Req2} = cowboy_req:body_qs(Req1),
-    lager:info("api:put id=~p, json=~p", [Id, from_json(Json)]),
+    {ok, Body, Req2} = cowboy_req:body(Req1),
+    lager:info("api:put id=~p, json=~p", [Id, from_json(Body)]),
     {true, Req2, State}.
 
-post_json(Req, State) ->
-    {ok, [Json], Req1} = cowboy_req:body_qs(Req),
-    lager:info("api:post json=~p", [from_json(Json)]),
+process_post(Req, State) ->
+    {ok, Body, Req1} = cowboy_req:body(Req),
+    lager:info("api:post json=~p", [from_json(Body)]),
     {true, Req1, State}.
 
 get_json(Req, State) ->
@@ -82,18 +72,13 @@ get_json(Req, State) ->
             end,
     {to_json(Reply), Req1, State}.
 
-delete_json(Req, State) ->
+delete_resource(Req, State) ->
     lager:info("api:delete", []),
     {true, Req, State}.
 
-%% process_post(Req, State) ->
-%%     {true, Req, State}.
-
-%% delete_resource(Req, State) ->
-%%     {true, Req, State}.
-
-%% delete_completed(Req, State) ->
-%%     {true, Req, State}.
+delete_completed(Req, State) ->
+    lager:info("api:delete_completed", []),
+    {true, Req, State}.
 
 %% Internal functions
 get_resource([Plugin]) ->
@@ -102,7 +87,6 @@ get_resource([Plugin]) ->
 get_resource([Plugin, Key]) ->
     % get a specific memory
     "".
-
 
 % Helpers
 from_json(Input) ->
