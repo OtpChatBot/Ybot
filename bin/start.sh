1de9720a5f02ffac41a382d76bf0c8e9e40d643a
Martin Wiso <martin@wiso.cz>
2014-03-12 12:04:18 +0100

    Fix wrong path to configration file from ops CLI

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kill $(ps x | grep 'skype.py' |  awk '/python/' | awk '{print $1}')
-cp ybot.config ../ebin
+cp ../ybot.config ../ebin
 
 erl -pa ../deps/*/ebin ../ebin \
     -boot start_sasl +P 2000000 \
