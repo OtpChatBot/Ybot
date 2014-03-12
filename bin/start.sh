e5a6b344f745c10699745a1f92123077a5d09bc4
Martin Wiso <martin@wiso.cz>
2014-03-12 16:03:25 +0100

    Fix logic for killing skype.py before staring node

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-kill $(ps x | grep 'skype.py' |  awk '/python/' | awk '{print $1}')
-cp ybot.config ../ebin
+SKYPE_PID=$(ps x | grep 'skype.py' |  awk '/python/' | awk '{print $1}')
+if [ -n "$SKYPE_PID" ]; then
+   kill $SKYPE_PID
+fi
+
+cp ybot.config ebin/
 
 erl -pa deps/*/ebin ebin \
     -boot start_sasl +P 2000000 \
     -sname ybot@$(hostname) \
-    -noshell \
-    -detached \
     -s ybot \
-    -config ybot
+    -config ybot \
+    -noshell \
+    -detached
