24290b252c0887c4622a66b30ae94650fe602a0f
Martin Wiso <martin@wiso.cz>
2014-03-12 12:03:17 +0100

    Add scripts for managin ybot nodes from CLI

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kill $(ps x | grep 'skype.py' |  awk '/python/' | awk '{print $1}')
-cp ybot.config `pwd`/ebin
-exec erl -pa ebin/ deps/*/ebin plugins/*/ebin -s ybot -config ybot -s reloader -boot start_sasl +P 2000000
