0ad306fef3cde93171d4bfd2eb99bd383694032f
Martin Wiso <martin@wiso.cz>
2014-03-14 12:14:26 +0100

    Clenup manager and plugin observer

--- src/ybot_plugins_observer.erl
+++ src/ybot_plugins_observer.erl
@@ -5,11 +5,11 @@
 %%% @end
 %%%-----------------------------------------------------------------------------
 -module(ybot_plugins_observer).
- 
+
 -behaviour(gen_server).
- 
+
 -export([start_link/3, stop/0]).
- 
+
 %% gen_server callbacks
 -export([init/1,
          handle_call/3,
@@ -21,7 +21,7 @@
 %% Public api
 -export([observe_new_plugins/2]).
 
-%% Internal state 
+%% Internal state
 -record(state, {
     % current loaded plugins
     current_plugins = [],
@@ -30,6 +30,7 @@
     % plugins directory
     plugins_directory = ""
     }).
+
 
 %%=============================================================================
 %% API functions
@@ -42,25 +43,27 @@
 %% Timeout - observe timeout
 %%
 start_link(PluginsDirectory, Plugins, Timeout) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [PluginsDirectory, Plugins, Timeout], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [PluginsDirectory, Plugins,
+                                                      Timeout], []).
 
 %% @doc stop observer process
 stop() ->
     gen_server:cast(ybot_plugins_observer, stop).
 
+
 %%%============================================================================
 %%% Observer callbacks
 %%%============================================================================
- 
+
 init([PluginsDirectory, Plugins, Timeout]) ->
-    % start observer
-    erlang:send_after(Timeout, self(), {check_new_plugins, PluginsDirectory, Plugins}),
-    % init observer parameters
-    {ok, #state{current_plugins = Plugins, timeout = Timeout, plugins_directory = PluginsDirectory}}.
+    erlang:send_after(Timeout, self(), check_new_plugins),
+
+    {ok, #state{current_plugins = Plugins, timeout = Timeout,
+                plugins_directory = PluginsDirectory}}.
 
 handle_call(get_observer_timeout, _From, State) ->
     {reply, State#state.timeout, State};
- 
+
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
 
@@ -75,63 +78,58 @@ handle_cast(_Msg, State) ->
     {noreply, State}.
 
 %% @doc check new plugins
-handle_info({check_new_plugins, PluginsDirectory, CurrentPlugins}, State) ->
-    % Get all plugins
-    Plugins = lists:append(
-                ybot_utils:get_all_files(PluginsDirectory),
-                ybot_utils:get_all_directories(PluginsDirectory)
-               ),
+handle_info(check_new_plugins,
+            #state{timeout = Timeout, plugins_directory = PluginsDirectory,
+                   current_plugins = CurrentPlugins} = State) ->
+
+    %% get all plugins
+    Plugins = lists:append(ybot_utils:get_all_files(PluginsDirectory),
+                           ybot_utils:get_all_directories(PluginsDirectory)),
+
     % Try to get difference between plugins
     case Plugins -- CurrentPlugins of
-        % no new plugins
         [] ->
-            erlang:send_after(State#state.timeout, self(), {check_new_plugins, State#state.plugins_directory, Plugins});
+            no_new_plugins;
         NewPluginsPaths ->
-            % Make new plugins
-            NewPlugins = lists:flatten(lists:map(fun ybot_manager:load_plugin/1, NewPluginsPaths)),
-            % update manager's plugins list
-            gen_server:cast(ybot_manager, {update_plugins, NewPlugins}),
-            % Start new timer
-            erlang:send_after(State#state.timeout, self(), {check_new_plugins, State#state.plugins_directory, Plugins})
+            %% Make new plugins
+            NewPlugins = lists:flatmap(
+                           fun ybot_manager:load_plugin/1, NewPluginsPaths),
+
+            %% update manager's plugins list
+            gen_server:cast(ybot_manager, {update_plugins, NewPlugins})
     end,
-    % return
+
+    %% Start new timer
+    erlang:send_after(Timeout, self(), check_new_plugins),
+
     {noreply, State#state{current_plugins = CurrentPlugins}};
- 
+
 handle_info(_Info, State) ->
     {noreply, State}.
- 
+
 terminate(_Reason, _State) ->
     ok.
- 
+
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
- 
+
+
 %%%============================================================================
 %%% Internal functions
 %%%============================================================================
 
 %% @doc check observe new plugins after start or not.
 observe_new_plugins(PluginsDirectory, PluginsPaths) ->
-     % Try to get checking_new_plugins parameter from config
-    UseNewPlugins = case application:get_env(ybot, checking_new_plugins) of
-                        {ok, true} ->
-                            true;
-                        {ok, false} ->
-                            false;
-                        _ ->
-                            false
-                    end,
-            
-    % Check checking_new_plugins
-    case UseNewPlugins of
-        true ->
-            % Get new plugins checking timeout
+    %% Check checking_new_plugins if enabled in config
+    case application:get_env(ybot, checking_new_plugins) of
+        {ok, true} ->
+            %% Get new plugins checking timeout
             {ok, NewPluginsCheckingTimeout} = application:get_env(ybot, checking_new_plugins_timeout),
-            % Start new plugins observer
+
+            %% Start new plugins observer
             ybot_plugins_observer:start_link(PluginsDirectory, PluginsPaths, NewPluginsCheckingTimeout);
         _ ->
-            % don't use new plugins
+            %% don't use new plugins
             pass
     end,
-    % return
     ok.
